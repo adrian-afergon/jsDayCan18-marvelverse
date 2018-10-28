790ca40453ecb192e9289c2294a0724495f89344
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-10-28 16:36:16 +0000

    feat(Server): add build to git.

---
+++ build/src/app-server.js
@@ -0,0 +1,53 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const express = require("express");
+const SocketIo = require("socket.io");
+class AppServer {
+    constructor() {
+        this.createApp();
+        this.config();
+        this.createServer();
+        this.sockets();
+        this.listen();
+    }
+    createApp() {
+        this.app = express();
+    }
+    createServer() {
+        this.server = http_1.createServer(this.app);
+    }
+    config() {
+        this.port = process.env.PORT || AppServer.PORT;
+    }
+    sockets() {
+        this.io = SocketIo(this.server);
+    }
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log('Running server on port %s', this.port);
+        });
+        this.io.on('connect', (socket) => {
+            console.log('Connected client on port %s.', this.port);
+            socket.on('disconnect', () => {
+                console.log('Client disconnected');
+            });
+            socket.on('snap', () => {
+                const clients = this.io.clients();
+                const connectedClientIds = Object.keys(clients.connected);
+                connectedClientIds.forEach((id) => {
+                    console.log('id:', id, connectedClientIds.indexOf(id) % 2 === 0);
+                    if (connectedClientIds.indexOf(id) % 2 === 0) {
+                        clients.connected[id].disconnect(true);
+                    }
+                });
+            });
+        });
+    }
+    getApp() {
+        return this.app;
+    }
+}
+AppServer.PORT = 8080;
+exports.AppServer = AppServer;
+//# sourceMappingURL=app-server.js.map
